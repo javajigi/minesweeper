b12e1976e8b40a839f26be185d0b9a0cf3b61afd
Alek <alekjin@me.com>
2013-11-03 15:40:51 +0900

    Main Create

--- src/main/java/Minesweeper/Main.java
+++ src/main/java/Minesweeper/Main.java
@@ -1,4 +1,22 @@
 package Minesweeper;
 
+import static Minesweeper.GameMaster.*;
+
 public class Main {
+	public static void main(String[] args) {
+		System.out.println("Game Start");
+
+		// X축, Y축, Mine갯수 받고 Board create, Mine set
+
+		Board gameBoard = new Board(1, 1, 1);
+
+		while (GameMaster.checkStatus(gameBoard)==GameStatus.GAMING)
+		{
+
+		}
+
+		// win
+
+		// lose
+	}
 }

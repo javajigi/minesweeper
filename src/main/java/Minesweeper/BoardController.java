8e0cb24ccb35903c597f7d7642ca5b1d444c688b
Alek <alekjin@me.com>
2013-11-05 19:16:04 +0900

    Board Print Done

--- src/main/java/Minesweeper/BoardController.java
+++ src/main/java/Minesweeper/BoardController.java
@@ -2,18 +2,19 @@ package Minesweeper;
 
 public interface BoardController {
 
-	void setMine (int xAxis, int yAxis, boolean mine);
-	void setFlag (int xAxis, int yAxis, boolean flag);
-	void setShow (int xAxis, int yAxis, boolean show);
+	void setMine(int xAxis, int yAxis, boolean mine);
+	void setFlag(int xAxis, int yAxis, boolean flag);
+	void setShow(int xAxis, int yAxis, boolean show);
 
 	void openBlock(int xAxis, int yAxis);
 
-	Block getBlock (int xAxis, int yAxis);
+	Block getBlock(int xAxis, int yAxis);
 
 	int getNumofInvisibleBlocks();
 	int getNumofFlagedBlocks();
 	int getNumofMines();
 	int getNumofBlocks();
+
 	int getXNum();
 	int getYNum();
 
